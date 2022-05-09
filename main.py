5ac3bedc13a09f0620b9d05d4e2835121e7533e2
ot07 <ot07neoki@gmail.com>
2022-05-10 02:10:42 +0900

    Add hello_world endpoint

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/hello_world")
+async def root():
+    return {"message": "Hello World"}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
